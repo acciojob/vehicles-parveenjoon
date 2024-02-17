4ef530f394f166017b72eacdb16807282c4a6184
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-02-17 18:44:44 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,29 +6,16 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
+    public String getName() {
+        return name;
+    }
+
     public Vehicle(String name) {
         this.name = name;
         this.currentSpeed = 0;
         this.currentDirection = 0;
     }
 
-    public void steer(int direction) {
-        currentDirection += direction;
-        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
-    }
-
-    public void move(int speed, int direction) {
-        this.currentSpeed = speed;
-        this.currentDirection = direction;
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
-    }
-
-    public void stop() {
-        this.currentSpeed = 0;
-        System.out.println("stop method called - The vehicle is stopped");
-    }
-
-    // Getter methods
     public int getCurrentSpeed() {
         return currentSpeed;
     }
@@ -36,4 +23,23 @@ public class Vehicle {
     public int getCurrentDirection() {
         return currentDirection;
     }
+
+    public void steer(int direction){
+        currentDirection = currentDirection+direction;
+        //direction is in degrees, add it to the current direction
+        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    }
+
+    public void move(int speed, int direction){
+        currentSpeed = speed;
+        currentDirection = direction;
+        //set the values of currentSpeed and currentDirection
+        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    }
+
+    public void stop(){
+       currentSpeed = 0;
+        System.out.println("stop method called - The vehicle is stopped");
+    }
+
 }
